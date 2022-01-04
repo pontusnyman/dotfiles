8f0f54f63ecd3fcb70ee7c2bb2dc27d0f0674854
Pontus Nyman <17062233+pontusnyman@users.noreply.github.com>
2022-01-04 21:34:52 +0100

    boom

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -4,8 +4,26 @@ echo "\n<<<Starting ZSH Setup >>>\n"
 
 # Installation unnecessary: its in th Brewfile.
 
-echo "Enter superuser (sudo password to edit /etc/shells"
-echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+if grep -Fxq '/usr/local/bin/zsh' '/etc/shells'; then
+  echo '/usr/local/bin/zsh already exits in /etc/shells'
+else
+  echo "Enter superuser (sudo password to edit /etc/shells"
+  echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+fi
 
-echo "Enter user password to to change shell"
-chsh -s '/usr/local/bin/zsh'
+if [ "$SHELL" = '/usr/local/bin/zsh' ]; then
+  echo "$Shell is already /usr/local/bin/zsh"
+else
+  echo "Enter user password to change login shell"
+  chsh -s '/usr/local/bin/zsh'
+fi
+
+if sh --version | greo -q zsh; then
+  echo '/private/var/select/sh already linked to /bin/zsh'
+else
+  echo "Enter superuser (sudo password to sumlink sh to zsh"
+  # locked okey, might delete later, idk
+  sudo ln -sfv /bin/zsh /private/var/select/sh
+fi
+
+
